a9a1dd1c2139e4f351b907f2bec29b703e80b6de
doeunhye <ehdmsgp123@naver.com>
2018-11-22 16:37:14 +0900

    단어 검색

--- main.c
+++ main.c
@@ -4,20 +4,28 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	FILE *fp;
+	char word[100];
+	char filename[100];
+	char input[100];
 	
-	FILE *fp = NULL;
-	char input;
-	int i;
+	printf("file name : ");
+	scanf("%s", filename);
 	
+	fp = fopen(filename, "r");
 	
+	printf("input a word to find : ");
+	scanf("%s", word);
 	
-	fp = fopen("sample.txt", "r");
-	//printf("fp:%i\n", fp);
-	while((input=fgetc(fp)) != EOF)
+	while(fgets(input,100,fp)!=NULL)
 	{
-		putchar (input);
+		if (strncmp(input,word,strlen(word)) == 0)
+		{
+			printf("find a word %s\n", input);
+		}
 	}
-	fclose(fp);
-	 
+	
+	printf("search done!");
+	
 	return 0;
 }
